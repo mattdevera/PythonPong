334c1b6ff5b65232aa8702ff229633897295b471
Matthew De Vera <deveramatthew1@gmail.com>
2024-07-10 13:29:06 -0700

    Implemented the scoreboard and timer display.

--- Pong.py
+++ Pong.py
@@ -16,3 +16,16 @@ border.pendown()
 for _ in range(4):
     border.forward(600)
     border.left(90)
+#scoreboard and timer
+score = 0
+seconds = 0
+scoreboard = Turtle(visible = False)
+scoreboard.color("white")
+scoreboard.penup()
+scoreboard.setposition(-290, 310)
+scoreboard.write("Score {}".format(score), False, align = "left", font = ("Arial", 16, "normal"))
+timer = Turtle(visible = False)
+timer.color("white")
+timer.penup()
+timer.setposition(260, 310)
+timer.write("Time {}".format(seconds), False, align = "left", font = ("Arial", 16, "normal"))
