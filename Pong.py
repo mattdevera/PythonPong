7809df19e618dedfb6b4339a2319f8587fb5c976
Matthew De Vera <deveramatthew1@gmail.com>
2024-07-10 13:22:11 -0700

    Implemented the screen and border.

--- Pong.py
+++ Pong.py
@@ -1,1 +1,18 @@
 from turtle import Turtle, Screen
+
+#implementation for the display
+#screen
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong")
+#border
+border = Turtle(visible = False)
+border.speed("fastest")
+border.color("white")
+border.pensize(3)
+border.penup()
+border.setposition(-300, 300)
+border.pendown()
+for _ in range(4):
+    border.forward(600)
+    border.left(90)
