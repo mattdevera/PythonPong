22d6e4e8c998ef6f417560587f9fc46207167b9c
Matthew De Vera <deveramatthew1@gmail.com>
2024-07-15 15:08:03 -0700

    Added attributes to the paddles and ball.

--- Pong.py
+++ Pong.py
@@ -39,6 +39,7 @@ player.color("red")
 player.penup()
 player.setposition(-280, -250)
 player.showturtle()
+playerSpeed = 15
 #computer paddle
 computer = Turtle("square", visible = False)
 computer.shapesize(0.5, 3)
@@ -48,6 +49,7 @@ computer.color("blue")
 computer.penup()
 computer.setposition(280, 250)
 computer.showturtle()
+computerSpeed = 15
 #ball
 ball = Turtle("circle", visible = False)
 ball.speed("fast")
@@ -55,3 +57,6 @@ ball.color("white")
 ball.penup()
 ball.sety(265)
 ball.showturtle()
+ballSpeed = 15
+ballChangeInX = 5
+ballChangeInY = -5
